22f3be0dd8687c95fa3b0231931886585780224c
xiangyang2 <15138778469@163.com>
2020-03-27 14:50:21 +0800

    aa

---
+++ src/main/java/com/xiangyang/test/IntSum.java
@@ -0,0 +1,12 @@
+package com.xiangyang.test;
+
+public class IntSum {
+    public static void main(String[] args) {
+        Long intSum = 0l;
+        for(int i=1;i<2;i++){
+            intSum += i;
+
+        }
+        System.out.print(intSum);
+    }
+}
