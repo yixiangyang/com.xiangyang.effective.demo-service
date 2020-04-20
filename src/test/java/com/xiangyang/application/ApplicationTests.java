2e85d86251b1eabc3ce80680771121b3d5a8b0cb
xiangyang2 <15138778469@163.com>
2020-04-20 11:39:47 +0800

    aa

--- src/test/java/com/xiangyang/application/ApplicationTests.java
+++ src/test/java/com/xiangyang/application/ApplicationTests.java
@@ -4,7 +4,11 @@ import com.xiangyang.model.TestUser;
 import com.xiangyang.service.TestUserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
@@ -22,8 +26,11 @@ public class ApplicationTests {
 		TestUser t = new TestUser();
 		t.setName("这个是测试3");
 		testUserService.save(t);
+		Pageable pageable = PageRequest.of(0, 5);
+		Page<TestUser> testUsers = testUserService.findAll(pageable);
+		
+		testUsers.getContent().forEach(System.out::println);
 	}
-
 	@Test
 	public void aa(){
 		TestUser t = new TestUser();
