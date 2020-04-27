d0cd729757757b9a5b9f433a49924f92651e186b
xiangyang2 <15138778469@163.com>
2020-04-27 16:51:06 +0800

    aa

--- src/main/java/com/xiangyang/controller/TestUserController.java
+++ src/main/java/com/xiangyang/controller/TestUserController.java
@@ -2,11 +2,20 @@ package com.xiangyang.controller;
 
 import com.xiangyang.model.TestUser;
 import com.xiangyang.service.TestUserService;
+import com.xiangyang.util.ExcelFormat;
+import com.xiangyang.util.ExcelHeaderInfo;
+import com.xiangyang.util.ExcelUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class TestUserController {
@@ -18,5 +27,17 @@ public class TestUserController {
         TestUser testUser = new TestUser();
         testUser.setName(name);
         testUserService.save(testUser);
+    }
+
+    @GetMapping("/aaa")
+    public void getAA(HttpServletResponse response){
+        List<TestUser> list = testUserService.findAll();
+        ExcelUtils excelUtils = new ExcelUtils(list,getHeaderInfo(),new HashMap<String, ExcelFormat>());
+        excelUtils.sendHttpResponse(response,"name",excelUtils.getWorkbook());
+    }
+
+    // 获取表头信息
+    private List<ExcelHeaderInfo> getHeaderInfo() {
+        return Arrays.asList(new ExcelHeaderInfo(0,0,0,0,"id"),new ExcelHeaderInfo(0,0,1,1,"名字"));
     }
 }
